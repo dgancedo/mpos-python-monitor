6d10c9cccfac31f1e77202b53bac5b849991b365
Diego S. Gancedo <dgancedo@Narsil.local>
2018-03-16 14:15:07 -0300

    Added html output, total hash/share rate and column order in the output

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Simple Python script to monitor account balance and workers status using mpos-ap
 
 Modifiy the script and assing your key and userid in the variables, you can get the api and user id in the pool webseite under my account => edit account.
 
+usage: python3 get-status.py
--- get-status.py
+++ get-status.py
@@ -7,12 +7,21 @@ def getmposapi(url,key,userid,action):
     data = json.loads(response.content)
     return data
 
-key = "yourapikey"
-userid = "youruserid"
-url= "https://vrm.n3rd3d.com/index.php"
+def drawline(message):
+    chars = len(message.translate(dict.fromkeys(range(32))))
+    for i in range(1,int(chars)): 
+        sys.stdout.write("-")
+        i=i+1
+    sys.stdout.write("\n")
 
-shownonmonitored = 0 #set to 1 if you want to show workers set as non monitored
-colors = 0 #set to 1 if you want colored output
+key = sys.argv[1] # put your api key ir pass it as firsth argument.
+userid = sys.argv[2] #put your user id or pass it as second argument.
+url = "https://vrm.n3rd3d.com/index.php"
+
+shownonmonitored = 1 #set to 1 if you want to show workers set as non monitored
+showtotals = 1 #show total hashrate/sharerate after the workers
+colors = 1 #set to 1 if you want colored output
+html = 0 #se to 1 to get html output.
 
 if colors == 1:
     red = '\x1b[1;31m'
@@ -29,19 +38,61 @@ else:
 
 
 data = getmposapi(url,key,userid,"getuserbalance")
-confirmedbalance = data['getuserbalance']['data']['confirmed'][:6]
-unconfirmedbalance = data['getuserbalance']['data']['unconfirmed'][:6]
-orphanedbalance = data['getuserbalance']['data']['orphaned'][:6]
+confirmedbalance = data['getuserbalance']['data']['confirmed'].split('.')[0] + "." + data['getuserbalance']['data']['confirmed'].split('.')[1][0:2]
+unconfirmedbalance = data['getuserbalance']['data']['unconfirmed'].split('.')[0] + "." + data['getuserbalance']['data']['unconfirmed'].split('.')[1][0:2]
+orphanedbalance = data['getuserbalance']['data']['orphaned'].split('.')[0] + "." + data['getuserbalance']['data']['orphaned'].split('.')[1][0:2]
 
-sys.stdout.write("Account balance: Confirmed: " + green + confirmedbalance + normal + "\tUnconfirmed: " + yellow + unconfirmedbalance + normal + "\tOrphaned: " + red + orphanedbalance + normal + "\n\n")
+if html == 1:
+    message = ("<html><body>Account balance: Configrmed: <font color=\"green\">" + confirmedbalance + "</font>    Unconfirmed: <font color=\"yellow\">" \
+    + unconfirmedbalance +"</font>    Orphaned: <font color=\"red\">" + orphanedbalance +"</font><br><hr><br>")
+else:
+    message = "Account balance: Confirmed: " + green + confirmedbalance + normal + "\tUnconfirmed: " + yellow + unconfirmedbalance + normal \
+    + "\tOrphaned: " + red + orphanedbalance + normal + "\n\n"
+    
+sys.stdout.write(message)
+if html != 1: drawline(message)
 
-print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=- Workers Status -=-=-=-=-=-=-=-=-=-=-=-=-=-=-") 
+
 data = getmposapi(url,key,userid,"getuserworkers")
+if html == 1:
+    sys.stdout.write("<table>")
 for worker in data['getuserworkers']['data']:
     if ((worker['monitor'] == 1) or (shownonmonitored == 1)):
         workername = worker['username'].split('.')[1]
-        if worker['hashrate'] == 0: 
-            color = red 
-        else: 
-            color = green
-        sys.stdout.write("Worker: "+ color + workername + normal+ "\tHashrate: " + str(worker['hashrate'])[:12] + "\tShares: " + str(worker['shares'])[:6] + "\tdifficulty: " + str(worker['difficulty'])[:6] + "\n")
+        workerhashrate =  str(float("{0:.2f}".format(worker['hashrate'])))
+        workersharerate = str(float("{0:.2f}".format(worker['shares'])))
+        workerdificulty = str(float("{0:.2f}".format(worker['difficulty'])))
+        if html == 1:
+            if worker['hashrate'] == 0:
+                color = "red"
+            else:
+                color = "green"
+            sys.stdout.write("<tr><td>Worker:</td><td><font color=\"" + color + "\">" + workername + "</font></td><td>   </td><td>Hashrate:</td><td>" \
+            + workerhashrate + "</font></td><td>   </td><td>Shares:</td><td>" + workersharerate + "</td><td>   </td><td>Difficulty:</td><td>" \
+            + workerdificulty + "</td></tr>")
+        else:
+            if worker['hashrate'] == 0: 
+                color = red 
+                hashtab = "\t"
+            else: 
+                color = green
+                hashtab = ""
+            if len(workername) < 8:
+                extratab = "\t"
+            else:
+                extratab = ""
+            
+            sys.stdout.write("Worker: " + color + workername + normal + extratab + "\tHashrate: " + workerhashrate + hashtab +"\tShares: " \
+            + workersharerate + "\tDifficulty: " + workerdificulty + "\n")
+
+if showtotals == 1:
+    userhashrate = str(float("{0:.2f}".format(getmposapi(url,key,userid,"getuserhashrate")['getuserhashrate']['data'])))
+    usersharerate = str(float("{0:.2f}".format(getmposapi(url,key,userid,"getuserhashrate")['getuserhashrate']['data'])))
+    if html == 1:
+        sys.stdout.write("<table><tr><td>Total Hashrate:</td><td>" + userhashrate  + "</td><td>Total Sharerate:</td><td>" + usersharerate  + "</td></tr></table>" )
+    else:
+        drawline(message)
+        sys.stdout.write("Total hashrate: " + userhashrate  + " Total Sharerate: " + usersharerate )
+if html == 1:
+    sys.stdout.write("</table></body></html>")
+                        
